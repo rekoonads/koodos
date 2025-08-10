7a1246d26c0513a65a49596cc6265c3f5beb7808
KOODOS Developer <koodos@example.com>
2025-08-10 15:22:50 +0530

    Update to Prisma Accelerate for live database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
   var __prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.__prisma ?? new PrismaClient()
+export const prisma = globalThis.__prisma ?? new PrismaClient().$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalThis.__prisma = prisma
