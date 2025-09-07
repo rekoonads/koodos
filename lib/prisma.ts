3afea80ff79ee4c517064efeb077cc274744e4e1
KOODOS Admin <admin@koodos.in>
2025-09-07 21:49:26 +0530

    Align frontend with backend schema structure

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
-import { PrismaClient } from "@prisma/client/edge"
-import { withAccelerate } from "@prisma/extension-accelerate"
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export { prisma }
+
+// Use Prisma generated types
+export type { Article, User, Category } from '@prisma/client';
